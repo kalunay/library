9c973685591deaa076652a8cda1445728722d98a
kalunay <kalunay@gmail.com>
2021-05-01 23:28:06 +0300

    add menu

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -39,8 +39,17 @@ AppAsset::register($this);
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
+            //['label' => 'About', 'url' => ['/site/about']],
+            //['label' => 'Contact', 'url' => ['/site/contact']],
+            !Yii::$app->user->isGuest ? (
+                ['label' => 'Book', 'url' => ['/book']]
+            ) : (''),
+            !Yii::$app->user->isGuest ? (
+                ['label' => 'Employee', 'url' => ['/employee']]
+            ) : (''),
+            !Yii::$app->user->isGuest ? (
+                ['label' => 'Client', 'url' => ['/client']]
+            ) : (''),
             Yii::$app->user->isGuest ? (
                 ['label' => 'Login', 'url' => ['/site/login']]
             ) : (
@@ -52,7 +61,7 @@ AppAsset::register($this);
                 )
                 . Html::endForm()
                 . '</li>'
-            )
+            ),
         ],
     ]);
     NavBar::end();
